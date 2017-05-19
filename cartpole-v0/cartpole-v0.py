227b4e0ec5b263880f5403d7a782d0475a123069
p-Mart <phillip.martin012@gmail.com>
2017-05-19 15:02:49 -0400

    Added Target network.

--- cartpole-v0/cartpole-v0.py
+++ cartpole-v0/cartpole-v0.py
@@ -6,6 +6,47 @@ import random
 #OpenAI Gym code.
 env = gym.make('CartPole-v0')
 
+
+'''
+class ActorNetwork(object):
+
+	def __init__(self, sess, learning_rate):
+
+		NODES_H1 = 2
+		NODES_H2 = 2
+
+		self.sess = sess
+		self.learning_rate = learning_rate
+
+		self.keep_prob = tf.placeholder(tf.float32)
+
+		self.x = tf.placeholder(tf.float32, [None, 4])
+
+		W_h1 = tf.Variable(tf.truncated_normal([4, NODES_H1], stddev=0.1))
+		b_h1 = tf.Variable(tf.constant(0.1, shape=[NODES_H1]))
+		h1 = tf.nn.relu(tf.matmul(self.x, W_h1) + b_h1)
+		h1_drop = tf.nn.dropout(h1, self.keep_prob)
+
+		W_h2 = tf.Variable(tf.truncated_normal([NODES_H1, NODES_H2], stddev=0.1))
+		b_h2 = tf.Variable(tf.constant(0.1, shape=[NODES_H2]))
+		h2 = tf.nn.relu(tf.matmul(h1_drop, W_h2) + b_h2)
+		h2_drop = tf.nn.dropout(h2, self.keep_prob)
+
+		W_out = tf.Variable(tf.truncated_normal([NODES_H2, 2], stddev=0.1))
+		b_out = tf.Variable(tf.constant(0.1, shape=[2]))
+		self.output = tf.matmul(h2_drop, W_out) + b_out
+
+		self.get_action = tf.argmax(tf.nn.softmax(self.output), 1)
+
+	def getOutput(self):
+		return self.output
+
+	def getAction(self, observation):
+		return self.sess.run(self.get_action, feed_dict={
+			self.x : np.array(observation).reshape((1,4)),
+			self.keep_prob : 1.0})
+'''
+
 def debug():
 	print env.action_space
 	print env.observation_space
@@ -31,8 +72,11 @@ def run():
 	episodes = 20000
 	NODES_H1 = 2
 	NODES_H2 = 2
+	learning_rate = 1e-3
 
-	#Network architecture
+	sess = tf.InteractiveSession()
+	###########Actor Network#############
+	
 	x = tf.placeholder(tf.float32, [None, 4])
 	keep_prob = tf.placeholder(tf.float32)
 
@@ -53,22 +97,44 @@ def run():
 	b_out = tf.Variable(tf.constant(0.1, shape=[2]))
 
 	output = tf.matmul(h2_drop, W_out) + b_out
-
-	target = tf.placeholder(tf.float32, [None, 2])
-
 	get_action = tf.argmax(tf.nn.softmax(output), 1)
+	
+	#actor = ActorNetwork(sess, learning_rate)
 
-	cross_entropy = tf.reduce_mean(
-		tf.square(target - output))
+	###########Target Network#############
+	x_t = tf.placeholder(tf.float32, [None, 4])
+	gamma = tf.constant(0.5)
 
-	learning_rate = 1e-3
-	train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+	W_h1_t = tf.Variable(tf.truncated_normal([4, NODES_H1], stddev=0.1))
+	b_h1_t = tf.Variable(tf.constant(0.1, shape=[NODES_H1]))
 
-	sess = tf.InteractiveSession()
+	h1_t = tf.nn.relu(tf.matmul(x_t, W_h1_t) + b_h1_t)
+	h1_drop_t = tf.nn.dropout(h1_t, keep_prob)
+
+
+	W_h2_t = tf.Variable(tf.truncated_normal([NODES_H1, NODES_H2], stddev=0.1))
+	b_h2_t = tf.Variable(tf.constant(0.1, shape=[NODES_H2]))
+
+	h2_t = tf.nn.relu(tf.matmul(h1_drop_t, W_h2_t) + b_h2_t)
+	h2_drop_t = tf.nn.dropout(h2_t, keep_prob)
+
+	W_out_t = tf.Variable(tf.truncated_normal([NODES_H2, 2], stddev=0.1))
+	b_out_t = tf.Variable(tf.constant(0.1, shape=[2]))
+
+	target = tf.scalar_mul(gamma, (tf.matmul(h2_drop_t, W_out_t) + b_out_t))
+	get_action_t = tf.argmax(tf.nn.softmax(target), 1)
+
+	#target = tf.placeholder(tf.float32, [None, 2])
+
+
+	loss = tf.reduce_mean(tf.square(target - output))
+
+	
+	train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+	
 	sess.run(tf.global_variables_initializer())
 
 	tf.get_default_graph().finalize()
-
 
 	#Training loop
 	for episode in range(episodes):
@@ -90,10 +156,12 @@ def run():
 				action = env.action_space.sample()
 				observation, reward, done, info = env.step(action)
 			else:
+				
 				action = get_action.eval(feed_dict={
-								x:np.array(observation).reshape((1,4)), 
-								keep_prob:1.0})
-
+							x:np.array(observation).reshape((1,4)), 
+							keep_prob:1.0})
+				
+				#action = get_action(observation)
 				action = int(action[0])
 				observation, reward, done, info = env.step(action)
 
@@ -107,7 +175,7 @@ def run():
 					#Train if memory full
 					train_step.run(feed_dict={
 						x : np.array(experience_x),
-						target : np.array(experience_y), 
+						x_t: np.array(experience_x),
 						keep_prob : 0.5})
 					
 					experience_x = []
@@ -125,7 +193,7 @@ def run():
 
 					train_step.run(feed_dict={
 							x : np.array(experience_x), 
-							target : np.array(experience_y),
+							x_t: np.array(experience_x),
 							keep_prob : 0.5})
 
 					experience_x = []
